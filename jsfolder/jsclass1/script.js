ff75e5c992a4a529f110c1d59062dd2e9d83e183
ibrahimfaiza333-cmyk <ibrahimfaiza333@gmail.com>
2026-01-17 07:27:06 -0800

    js assigmnet added

---
+++ jsfolder/jsclass1/script.js
@@ -0,0 +1,247 @@
+// // ==================exercise 1==========
+
+// // task 1
+// alert("hello user");
+
+// // task2
+
+// alert("Eroor! please enter a valid password");
+
+
+// // task3
+// alert("welcome to js land...\nhappy coding");
+
+// // task 4
+
+// alert("welcome to js land");
+// alert("Happy Coding ! \n  prevent this page from creating additional dialogs");
+
+// // ============== assigmnet2===========
+
+// // task 1
+// var username;
+
+// //task 2
+// var myName="Faiza Ibrahim";
+
+// // task 3
+
+// var message="hello world";
+//  alert(message);
+
+// // task 4:
+//  var name="faiza";
+//  var age=19;
+//  var course="persuaing web development";
+
+//  alert(name);
+//  alert(age);
+//  alert(course);
+
+
+// //  task 5
+// var msg = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+//   alert(msg);
+
+// //   task 6
+// var email="faizaibrahim@gmail.com";
+
+// alert("my email id is :"+email);
+
+// // task 7
+
+// var book="a smarter way to learn javascript";
+// alert(book);
+
+
+// // task 8
+
+// document.write("Yah ! i can write html content through js ")
+
+
+
+// // task 9
+
+// var pattern="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(pattern);
+
+
+// // ============== class 3===========
+
+
+// // task 1
+
+// var myAge=19;
+// alert( "i am "+myAge +"years old");
+
+
+
+// // task 3
+
+// var birthYear=2006;
+// document.write("my birth yaear is "+birthYear+"<br>")
+// document.writeln("data type of my declared variable is "+typeof birthYear);
+
+
+// // task 4
+var visitor="John Doe";
+var producttitle="T-shirt";
+var quantity=5;
+document.writeln(visitor+" ordered" +quantity+producttitle+"(s) on XYZ Clotthing Store");
+
+
+// // ===============class5==============
+
+// // task 1
+
+// var a=3;
+// var b=4;
+// document.writeln("sum of "+a +"and " +b+"is "+a+b);
+// document.writeln("sum of "+a +"and " +b+"is "+a-b);
+// document.writeln("sum of "+a +"and " +b+"is "+a*b);
+// document.writeln("sum of "+a +"and " +b+"is "+a/b);
+
+
+// // task 2
+// var num;
+// document.writeln("value of variable is "+num);
+// num=2;
+// document.writeln("value of variable is "+num);
+// num++;
+// document.writeln("value of variable is "+num);
+// num+=7;
+// document.writeln("value of variable is after addition "+num);
+// num--;
+// document.writeln("value of variable is "+num);
+// var remain=num/3;
+// document.writeln("value of reaminder is "+remain);
+
+
+
+// // task 4
+// var ticket=600;
+// document.writeln("total cost to buy 5 ticket to a movie is"+ticket*5);
+
+// // task 5  for se easily hojata hai 
+// var mul=4;
+// for(var i=0;i<10;i++){
+//   document.writeln("4 x" +i+"="+mul+"<br>");
+// }
+
+// // task 6
+
+// var clculas=70;
+
+
+// var farenheirt=((clculas*9/5)+32);
+// document.writeln("70 is  "+farenheirt);
+
+
+//  farenheirt=40;
+
+// clculas=((farenheirt-32)*5/9);
+
+// document.writeln("40 is  "+clculas);
+
+
+// // task 7;
+// var item1=650;
+// var item2=500;
+// var shipping=100;
+// var total=item1*3+item2*4+shipping;
+
+
+// document.writeln("<h1>shopping cart</h2>")
+// document.writeln("price of item 1 is "+item1+"<br>");
+// document.writeln("quantity of item1 is  3"+"<br>")
+// document.writeln("")
+// document.writeln("price of item 2 is "+item2+"<br>");
+// document.writeln("quantity of item1 is  4 <br> ");
+// document.writeln("shipping charges is "+shipping+"</br>")
+// document.writeln("total cost of your order is "+total+"<br>");
+
+
+
+// // task  8
+
+// var marks=500;
+// var obtained=400;
+// var percentage =((obtained/marks)*100);
+
+// document.writeln("<h1>marksheet </h1>");
+
+// document.writeln("total marks "+marks+"<br>" );
+// document.writeln("total marks "+obtained +"</br>");
+// document.writeln("percentage "+percentage +"</br>");
+
+// // task 9
+
+
+
+// var usdollar=10*104.80;
+// var saudi=25*28;
+// var sum=usdollar+saudi;
+// document.writeln("<h1>Currency in PKR</h1> <br>")
+// document.writeln("total currency in PKR:"+sum)
+
+// // task10
+
+// var RandomNumber=88;
+// var sum=(((RandomNumber+5)*10)/2);
+// console.log("sum");
+
+
+
+// task 11
+
+
+// var currentYear=2026;
+// var birthYear=2005;
+
+// var Possibleage=currentYear-birthYear;
+// var possibleagepredict=Possibleage-1;
+//  console.log("the age may be "+possibleagepredict +"or " +Possibleage);
+ 
+
+//  task 12
+
+// const PI=3.142;
+// var raduis=30;
+
+// var circumferrence=2*pi*raduis;
+
+// var area=pi*pi*raduis;
+
+// document.writeln("<h1>THE GEOMETRIZER</h1>");
+// document.writeln("raduis of the circle is "+raduis);
+// document.writeln("circumference of the circle is "+circumferrence);
+// document.writeln("area of the circle is "+area);
+
+
+
+// tassk 13
+
+var snack="fugde cake";
+var currentAge=20;
+var maxAge=50;
+var maxQuantity=2;
+var total=30*365*2;
+
+
+
+
+
+document.write("<h1>The Lifetime Supply Calculator</h1><br>");
+
+  document.write("Favourite Snack: " + snack + "<br>");
+  document.write("Current age: " + currentAge + "<br>");
+  document.write("Estimated Maximum Age: " + maxAge + "<br>");
+  document.write("Amount of snacks per day: " + maxQuantity + "<br><br>");
+
+  document.write(
+    "You will need " + total + " " + snack +
+    " to last you until the ripe old age of " + maxAge
+  );
+
+
+
